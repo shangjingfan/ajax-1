fbeed7316a277f8c8aa950e25145658bf91b327c
shangjingfan <876625909@qq.com>
2021-02-03 20:56:37 +0800

    AJAX模拟分页操作

--- public/main.js
+++ public/main.js
@@ -1,16 +1,38 @@
 
 // console.log('我是main.js 123')
-getJSON.onclick = ()=>{
+let n = 1
+getPage.onclick = () => {
+  n += 1
+  if (n <= 3) {
+    const request = new XMLHttpRequest()
+    request.open('GET', `/page${n}`)
+    request.onreadystatechange = () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const array = JSON.parse(request.response)
+        array.forEach(item => {
+          const li = document.createElement('li')
+          li.textContent = item.id
+          ulList.appendChild(li)
+        })
+      }
+    }
+    request.send()
+  } else {
+    alert("最后一页，没了")
+  }
+}
+
+getJSON.onclick = () => {
   const request = new XMLHttpRequest()
   request.open('GET', '/5.json')
-  request.onreadystatechange = ()=>{
+  request.onreadystatechange = () => {
     // console.log(request.readyState)
-    if(request.readyState === 4){
-      if(request.status >= 200 && request.status < 300){
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
         let object
-        try{
-          object = JSON.parse(request.response) 
-        }catch(error){
+        try {
+          object = JSON.parse(request.response)
+        } catch (error) {
           console.log('出错了，错误详情是')
           console.log(error)
           object = 'FUN'
@@ -22,17 +44,17 @@ getJSON.onclick = ()=>{
   request.send()
 }
 
-getXML.onclick = ()=>{
+getXML.onclick = () => {
   const request = new XMLHttpRequest()
   request.open('GET', '/4.xml')
-  request.onreadystatechange = ()=>{
-    if(request.readyState === 4){
-      if(request.status >= 200 && request.status < 300){
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
         // console.log(request.responseXML) // dom对象
         const dom = request.responseXML
         const text1 = dom.getElementsByTagName('warning')[0].textContent
         console.log(text1.trim())
-      }else{
+      } else {
         alert('请求XML失败')
       }
     }
@@ -40,22 +62,22 @@ getXML.onclick = ()=>{
   request.send()
 }
 
-getHTML.onclick = ()=>{
+getHTML.onclick = () => {
   const request = new XMLHttpRequest()
   request.open('GET', '/3.html')
-  request.onload = ()=>{
+  request.onload = () => {
     const div = document.createElement('div')
     div.innerHTML = request.response
     document.body.appendChild(div)
   }
-  request.onerror = ()=>{}
+  request.onerror = () => { }
   request.send()
 }
 
-getJS.onclick = ()=>{
+getJS.onclick = () => {
   const request = new XMLHttpRequest()
   request.open('GET', '/2.js')
-  request.onload = ()=>{
+  request.onload = () => {
     // 创建script标签
     const script = document.createElement('script')
     // 填写script标签内容
@@ -63,29 +85,29 @@ getJS.onclick = ()=>{
     // 插到body里
     document.body.appendChild(script)
   }
-  request.onerror = ()=>{
+  request.onerror = () => {
     console.log('请求CSS失败')
   }
   request.send()
 }
 
-getCSS.onclick = ()=>{
+getCSS.onclick = () => {
   const request = new XMLHttpRequest()
-  request.open('GET', '/css') 
-  request.onreadystatechange = ()=>{ 
+  request.open('GET', '/css')
+  request.onreadystatechange = () => {
     //等于4代表加载完成，但不知道加载的是成功页面还是失败页面
-    if(request.readyState === 4){ 
+    if (request.readyState === 4) {
       // http状态码：200开头的百位数一般是成功
-      if(request.status >=200 && request.status < 300){
+      if (request.status >= 200 && request.status < 300) {
         console.log(request.response)
         const style = document.createElement('style')
         style.innerHTML = request.response
         document.head.appendChild(style)
-      }else{
+      } else {
         alert('加载css失败')
       }
     }
   }
-  request.send() 
+  request.send()
 }
 
--- server.js
+++ server.js
@@ -3,16 +3,16 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
+if (!port) {
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
+  var pathWithQuery = request.url
   var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
   var path = parsedUrl.pathname
   var query = parsedUrl.query
   var method = request.method
@@ -21,46 +21,62 @@ var server = http.createServer(function(request, response){
 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
-  if(path === '/index.html'){
+  if (path === '/index.html') {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     // 通过fs访问外部文件
-    const string = fs.readFileSync('public/index.html').toString()
+    let string = fs.readFileSync('public/index.html').toString()
     const page1 = fs.readFileSync('db/page1.json').toString()
-    // string = string.replace('{{page1}}', page1)
+    // 直接使用string.replace，会报错它不是一个function,所以后面加了toString(), 具体原因不知
+    console.log(typeof string)
+    console.log(typeof page1)
+    const array = JSON.parse(page1)
+    const result = array.map(item => `<li>${item.id}</li>`).join('')
+    string = string.replace('{{page1}}', `<ul id="ulList">${result}</ul>`)
     response.write(string)
     response.end()
-  } else if(path === '/main.js'){
+  } else if (path === '/main.js') {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
     // 通过fs访问外部文件
     response.write(fs.readFileSync('public/main.js'))
     response.end()
-  }else if(path === '/css'){
+  } else if (path === '/css') {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
     response.write(fs.readFileSync('public/style.css'))
     response.end()
-  }else if(path === '/2.js'){
+  } else if (path === '/2.js') {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
     response.write(fs.readFileSync('public/2.js'))
     response.end()
-  }else if(path === '/3.html'){
+  } else if (path === '/3.html') {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(fs.readFileSync('public/3.html'))
     response.end()
-  }else if(path === '/4.xml'){
+  } else if (path === '/4.xml') {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/xml;charset=utf-8')
     response.write(fs.readFileSync('public/4.xml'))
     response.end()
-  }else if(path === '/5.json'){
+  } else if (path === '/5.json') {
     response.statusCode = 200
     response.setHeader('Content-Type', 'application/json;charset=utf-8')
     response.write(fs.readFileSync('public/5.json'))
     response.end()
+  } else if (path === '/page2') {
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'application/json;charset=utf-8')
+    response.write(fs.readFileSync('db/page2.json'))
+    response.end()
+  } else if (path === '/page3') {
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'application/json;charset=utf-8')
+    response.write(fs.readFileSync('db/page3.json'))
+    response.end()
+
   } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
